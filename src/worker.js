e2d7f7048d7b39995f55aea072337fee043c48a4
Oleksandr Shtovba <jilexandr@gmail.com>
2025-08-26 13:17:08 +0300

    remove xid-js

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,5 @@
 import { parse as parseCookie, serialize as serializeCookie } from 'cookie';
 
-import xid from 'xid-js';
-
 const PATH = '/p.js';
 
 export default {
@@ -43,7 +41,7 @@ export default {
       });
 
       if (!visitorId) {
-        visitorId = xid.next();
+        visitorId = genXid();
         const tokenValue = await genToken(keyBytes, visitorId);
 
         // use cookie.serialize for correctness
@@ -159,28 +157,28 @@ async function parseVisitorId(keyBytes, token) {
 }
 
 // ----- XID-like (20-char base32hex) -----
-// function genXid() {
-//   const buf = new Uint8Array(12);
-//   const nowSec = Math.floor(Date.now() / 1000);
-//   buf[0] = (nowSec >>> 24) & 0xff;
-//   buf[1] = (nowSec >>> 16) & 0xff;
-//   buf[2] = (nowSec >>> 8) & 0xff;
-//   buf[3] = nowSec & 0xff;
-//   crypto.getRandomValues(buf.subarray(4));
-//   return base32HexEncode(buf);
-// }
-//
-// function base32HexEncode(bytes) {
-//   const alphabet = '0123456789abcdefghijklmnopqrstuv';
-//   let bits = 0, value = 0, out = '';
-//   for (let i = 0; i < bytes.length; i++) {
-//     value = (value << 8) | bytes[i];
-//     bits += 8;
-//     while (bits >= 5) {
-//       out += alphabet[(value >>> (bits - 5)) & 31];
-//       bits -= 5;
-//     }
-//   }
-//   if (bits > 0) out += alphabet[(value << (5 - bits)) & 31];
-//   return out;
-// }
+function genXid() {
+  const buf = new Uint8Array(12);
+  const nowSec = Math.floor(Date.now() / 1000);
+  buf[0] = (nowSec >>> 24) & 0xff;
+  buf[1] = (nowSec >>> 16) & 0xff;
+  buf[2] = (nowSec >>> 8) & 0xff;
+  buf[3] = nowSec & 0xff;
+  crypto.getRandomValues(buf.subarray(4));
+  return base32HexEncode(buf);
+}
+
+function base32HexEncode(bytes) {
+  const alphabet = '0123456789abcdefghijklmnopqrstuv';
+  let bits = 0, value = 0, out = '';
+  for (let i = 0; i < bytes.length; i++) {
+    value = (value << 8) | bytes[i];
+    bits += 8;
+    while (bits >= 5) {
+      out += alphabet[(value >>> (bits - 5)) & 31];
+      bits -= 5;
+    }
+  }
+  if (bits > 0) out += alphabet[(value << (5 - bits)) & 31];
+  return out;
+}
